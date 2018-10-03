2fb80b23521f506db04e1d884f88adce1398beba
Mike <13jmmq@gmail.com>
2018-10-02 20:33:49 -0600

    implemented user connections to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+User.create!(first_name: 'john',
+	          last_name: 'doe',
+	         email: 'test@test.com', 
+             password: '123456789', 
+             password_confirmation: '123456789')
+
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content")
 end
